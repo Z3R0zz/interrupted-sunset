a3bf15370b4900d275d608dfa9b45270be201d00
Kars <k@kars.bio>
2025-04-16 20:30:57 +0100

    feat: finish protected routes

---
+++ frontend/src/lib/server/protected.ts
@@ -0,0 +1,120 @@
+"use server";
+import api from "@/modules/API";
+import type { APIRes, UserRes } from "@/types/api";
+import axios from "axios";
+
+export async function getUser(): Promise<APIRes<UserRes>> {
+    try {
+        const res = await api.get<UserRes>("/user");
+        if (res.data instanceof Object) {
+            return {
+                success: true,
+                data: res.data,
+            };
+        }
+        return {
+            success: false,
+            error: "Failed to get user",
+        };
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            return {
+                success: false,
+                error: error.response?.data?.error || error.message,
+            };
+        }
+        return {
+            success: false,
+            error: "Failed to get user",
+        };
+    }
+}
+
+export async function createMail(
+    email: string,
+): Promise<APIRes<{ message: string }>> {
+    try {
+        const res = await api.post<{ message: string }>("/mail/new", {
+            email,
+        });
+        if (res.data.message == "Please check your email for the OTP code") {
+            return {
+                success: true,
+                data: res.data,
+            };
+        }
+        return {
+            success: false,
+            error: "Failed to create mail",
+        };
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            return {
+                success: false,
+                error: error.response?.data?.error || error.message,
+            };
+        }
+        return {
+            success: false,
+            error: "Failed to create mail",
+        };
+    }
+}
+
+export async function verifyMail(
+    code: string,
+): Promise<APIRes<{ message: string }>> {
+    try {
+        const res = await api.post<{ message: string }>("/mail/verify", {
+            code,
+        });
+        if (res.data.message == "Email verified successfully") {
+            return {
+                success: true,
+                data: res.data,
+            };
+        }
+        return {
+            success: false,
+            error: "Failed to verify mail",
+        };
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            return {
+                success: false,
+                error: error.response?.data?.error || error.message,
+            };
+        }
+        return {
+            success: false,
+            error: "Failed to verify mail",
+        };
+    }
+}
+
+export async function downloadQueue(): Promise<APIRes<{ message: string }>> {
+    try {
+        const res = await api.post<{ message: string }>("/queue");
+        if (res.data.message == "Queue entry created successfully") {
+            return {
+                success: true,
+                data: res.data,
+            };
+        }
+        return {
+            success: false,
+            error: "Failed to download queue",
+        };
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            return {
+                success: false,
+                error: error.response?.data?.error || error.message,
+            };
+        }
+        return {
+            success: false,
+            error: "Failed to download queue",
+        };
+    }
+}
