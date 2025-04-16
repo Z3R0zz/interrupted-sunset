ab86e55e8037c538f4f1ffb5197d8d941dfb3445
Z3R0zz <123938225+Z3R0zz@users.noreply.github.com>
2025-04-16 20:34:03 +0300

    Add database migration for OTP table and setup database connection

--- backend/src/database/database.go
+++ backend/src/database/database.go
@@ -2,7 +2,10 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 )
 
 var DB *sql.DB
@@ -17,4 +20,35 @@ func Connect(dsn string) {
 	if err = DB.Ping(); err != nil {
 		log.Fatal("Error pinging database:", err)
 	}
+
+	err = setUpDatabase()
+	if err != nil {
+		log.Fatal("Error setting up database:", err)
+	}
+}
+
+func setUpDatabase() error {
+	sqlPath := "src/database/migrations"
+	sqlFiles, err := filepath.Glob(filepath.Join(sqlPath, "*.sql"))
+	if err != nil {
+		return fmt.Errorf("error finding migration files: %w", err)
+	}
+
+	for _, file := range sqlFiles {
+		if err := executeSQLFile(file); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func executeSQLFile(filePath string) error {
+	sqlContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return fmt.Errorf("error reading %s: %w", filePath, err)
+	}
+	if _, err := DB.Exec(string(sqlContent)); err != nil {
+		return fmt.Errorf("error executing %s: %w", filePath, err)
+	}
+	return nil
 }
