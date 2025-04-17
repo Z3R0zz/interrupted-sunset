d1a23ebc4f9d7adca1c65abd04e752009a56a56d
Z3R0zz <123938225+Z3R0zz@users.noreply.github.com>
2025-04-17 18:44:47 +0300

    refactor: streamline logger initialization and enhance log level handling

--- backend/src/utils/logger.go
+++ backend/src/utils/logger.go
@@ -13,18 +13,41 @@ var Logger = logrus.New()
 
 func init() {
 	Logger.SetFormatter(&logrus.JSONFormatter{})
+	Logger.SetLevel(logrus.TraceLevel)
 
-	logFile := &lumberjack.Logger{
-		Filename:   "logs/app.log",
-		MaxSize:    10,
-		MaxBackups: 3,
-		MaxAge:     30,
-		Compress:   true,
+	Logger.AddHook(&LevelHook{
+		Writer:    os.Stdout,
+		LogLevels: []logrus.Level{logrus.InfoLevel, logrus.DebugLevel, logrus.TraceLevel},
+	})
+
+	Logger.AddHook(&LevelHook{
+		Writer: &lumberjack.Logger{
+			Filename:   "logs/app.log",
+			MaxSize:    10,
+			MaxBackups: 3,
+			MaxAge:     30,
+			Compress:   true,
+		},
+		LogLevels: []logrus.Level{logrus.WarnLevel, logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel},
+	})
+}
+
+type LevelHook struct {
+	Writer    io.Writer
+	LogLevels []logrus.Level
+}
+
+func (h *LevelHook) Fire(entry *logrus.Entry) error {
+	line, err := entry.String()
+	if err != nil {
+		return err
 	}
+	_, err = h.Writer.Write([]byte(line))
+	return err
+}
 
-	multiWriter := io.MultiWriter(os.Stdout, logFile)
-	Logger.SetOutput(multiWriter)
-	Logger.SetLevel(logrus.InfoLevel)
+func (h *LevelHook) Levels() []logrus.Level {
+	return h.LogLevels
 }
 
 func HandleError(c *fiber.Ctx, err error, status int, message string) error {
